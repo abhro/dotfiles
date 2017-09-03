da0423286e0bfe6c39215b720d2d0924b5938ecc
Abhro <Abhro@users.noreply.github.com>
2017-09-04 03:26:07 +0600

    Add powershell profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,33 @@
+Set-StrictMode -Version Latest
+
+function Get-Types   # doesn't provide any real value. just to play around with.
+{
+    [System.AppDomain]::CurrentDomain.GetAssemblies() | Foreach-Object GetTypes
+}
+
+function Invoke-Speech
+{
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+        [string] $Say
+    )
+
+    Add-Type -AssemblyName System.Speech
+    $voice = New-Object System.Speech.Synthesis.SpeechSynthesizer
+
+    process { $voice.Speak($Say) > $null }
+}
+
+
+function idle
+{
+    pythonw.exe -m idlelib $args
+}
+
+function pydoc
+{
+    python.exe -m pydoc $args
+}
+
+New-Alias -Name Out-Voice -Value Invoke-Speech
