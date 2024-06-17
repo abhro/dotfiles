8e01a43f898f6a0309f74847c829b43e89b94eda
abhro <5664668+abhro@users.noreply.github.com>
2024-06-17 09:57:03 -0400

    Update startup.jl
    - Add stack trace package to startup.jl
    - Move imports to go in atreplinit()

--- startup.jl
+++ startup.jl
@@ -1,1 +1,9 @@
 # Julia (Julialang) REPL startup file
+# Should be placed in ~/.julia/config/startup.jl
+
+atreplinit() do repl
+    @eval using Revise, OhMyREPL
+    redirect_stdio(stderr=devnull) do
+        @eval using AbbreviatedStackTraces
+    end
+end
