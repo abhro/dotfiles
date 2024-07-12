59f8a6a024d9e98f4d5e8092f6ecb55a08e778ae
abhro <5664668+abhro@users.noreply.github.com>
2024-07-12 12:17:42 -0400

    Move imports to go in atreplinit()

--- startup.jl
+++ startup.jl
@@ -1,8 +1,9 @@
 # Julia (Julialang) REPL startup file
 # Should be placed in ~/.julia/config/startup.jl
 
-using Revise
-using OhMyREPL
-redirect_stdio(stderr=devnull) do
-    @eval using AbbreviatedStackTraces
+atreplinit() do repl
+    @eval using Revise, OhMyREPL
+    redirect_stdio(stderr=devnull) do
+        @eval using AbbreviatedStackTraces
+    end
 end
