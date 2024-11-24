0fb3dfab9e9fead1fdfb7ede54f17daf50d3ea69
abhro <5664668+abhro@users.noreply.github.com>
2024-11-24 12:01:34 -0500

    Add Ctrl+d to exit handler on powershell

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,7 @@
 Set-StrictMode -Version Latest
 Set-PSReadlineOption -HistorySaveStyle SaveNothing -BellStyle None -HistoryNoDuplicates -ShowToolTips
 Set-PSReadlineKeyHandler -Key Tab -Function Complete
+Set-PSReadlineKeyHandler -Key ctrl+d -Function ViExit
 # Extra keybindings and options for PowerShell Core on Linux
 #Set-PSReadlineKeyHandler -Key 'Ctrl+LeftArrow' -Function ShellBackwardWord
 #Set-PSReadlineKeyHandler -Key 'Ctrl+RightArrow' -Function ShellForwardWord
