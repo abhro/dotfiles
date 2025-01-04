f46c2801730f7335f387516156959a155ef46d13
abhro <5664668+abhro@users.noreply.github.com>
2025-01-04 13:20:49 -0500

    Remove functions from Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,40 +1,14 @@
 Set-StrictMode -Version Latest
+
 Set-PSReadlineOption -HistorySaveStyle SaveNothing -BellStyle None -HistoryNoDuplicates -ShowToolTips
+
 Set-PSReadlineKeyHandler -Key Tab -Function Complete
-Set-PSReadlineKeyHandler -Key ctrl+d -Function ViExit
+Set-PSReadlineKeyHandler -Key Ctrl+D -Function ViExit
+
 # Extra keybindings and options for PowerShell Core on Linux
 #Set-PSReadlineKeyHandler -Key 'Ctrl+LeftArrow' -Function ShellBackwardWord
 #Set-PSReadlineKeyHandler -Key 'Ctrl+RightArrow' -Function ShellForwardWord
 #Set-PSReadlineOption -HistorySavePath ~/.powershell_history
 
-function Get-Types   # doesn't provide any real value. just to play around with.
-{
-    [System.AppDomain]::CurrentDomain.GetAssemblies() | Foreach-Object GetTypes
-}
-
-function Invoke-Speech
-{
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
-        [string] $Say
-    )
-
-    Add-Type -AssemblyName System.Speech
-    $voice = New-Object System.Speech.Synthesis.SpeechSynthesizer
-
-    process { $voice.Speak($Say) > $null }
-}
-
-
-function idle
-{
-    pyw.exe -m idlelib $args
-}
-
-function pydoc
-{
-    py.exe -m pydoc $args
-}
-
-New-Alias -Name Out-Voice -Value Invoke-Speech
+function idle { pyw.exe -m idlelib $args }
+function pydoc { py.exe -m pydoc $args }
