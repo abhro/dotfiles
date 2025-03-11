744eca2fbd17a175821f423595bc92ab392b47aa
abhro <5664668+abhro@users.noreply.github.com>
2025-03-11 16:32:47 -0400

    Update startup.jl

--- startup.jl
+++ startup.jl
@@ -1,6 +1,8 @@
 # Julia (Julialang) REPL startup file
 # Should be placed in ~/.julia/config/startup.jl
 
+import Pkg
+
 atreplinit() do repl
     @eval using Revise, OhMyREPL
     redirect_stdio(stderr=devnull) do
@@ -11,29 +13,29 @@ end
 function template()
     @eval begin
         using PkgTemplates
-        tpl = Template(
+        plugins = [
+            Citation(),
+            Codecov(),
+            CompatHelper(cron = "0 0 */5 * *"),
+            Dependabot(),
+            Documenter{GitHubActions}(),
+            Git(),
+            GitHubActions(linux = true, windows = true, osx = true),
+            License(name = "MIT"),
+            PkgEvalBadge(),
+            ProjectFile(version = v"0.0.0-DEV"),
+            Readme(),
+            SrcDir(),
+            TagBot(),
+            Tests(project = true, aqua = true),
+        ]
+        Template(;
             user = "abhro",
             authors = "Abhro R. and contributors",
             dir = joinpath(homedir(), "src"),
             host = "github.com",
             julia = v"1.10.0",
-            plugins = [
-                Citation(),
-                Codecov(),
-                CompatHelper(),
-                Dependabot(),
-                Documenter{GitHubActions}(),
-                Git(),
-                GitHubActions(),
-                License(name = "MIT"),
-                PkgBenchmark(),
-                PkgEvalBadge(),
-                ProjectFile(version = v"0.0.0-DEV"),
-                Readme(),
-                SrcDir(),
-                TagBot(),
-                Tests(project = true, aqua = true),
-            ],
+            plugins,
         )
     end
 end
