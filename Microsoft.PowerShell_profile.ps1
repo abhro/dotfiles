685560a3db1086c658a637d7cd4939b5ef5d3c41
abhro <5664668+abhro@users.noreply.github.com>
2025-04-02 10:20:07 -0400

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,16 @@
 Set-StrictMode -Version Latest
 
-Set-PSReadlineOption -HistorySaveStyle SaveNothing -BellStyle None -HistoryNoDuplicates -ShowToolTips
+Set-PSReadlineOption -HistorySaveStyle SaveNothing
+Set-PSReadlineOption -HistoryNoDuplicates
+Set-PSReadlineOption -BellStyle None -ShowToolTips
+
+Set-PSReadLineOption -AddToHistoryHandler {
+    param($command)
+    if ($command -like ' *') {
+        return $false
+    }
+    return $true
+}
 
 Set-PSReadlineKeyHandler -Key Tab -Function Complete
 Set-PSReadlineKeyHandler -Key Ctrl+D -Function ViExit
@@ -8,7 +18,7 @@ Set-PSReadlineKeyHandler -Key Ctrl+D -Function ViExit
 # Extra keybindings and options for PowerShell Core on Linux
 #Set-PSReadlineKeyHandler -Key 'Ctrl+LeftArrow' -Function ShellBackwardWord
 #Set-PSReadlineKeyHandler -Key 'Ctrl+RightArrow' -Function ShellForwardWord
-#Set-PSReadlineOption -HistorySavePath ~/.powershell_history
+#Set-PSReadlineOption -HistorySavePath ~/.pwsh_history
 
 function idle { pyw.exe -m idlelib $args }
 function pydoc { py.exe -m pydoc $args }
