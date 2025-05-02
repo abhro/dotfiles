4c6918146475a0e49e538014304b1ef9718bb71f
abhro <5664668+abhro@users.noreply.github.com>
2025-05-02 08:33:39 -0400

    Update startup.jl

--- startup.jl
+++ startup.jl
@@ -2,6 +2,7 @@
 # Should be placed in ~/.julia/config/startup.jl
 
 atreplinit() do repl
+    @eval import Pkg
     @eval using Revise, OhMyREPL
     redirect_stdio(stderr=devnull) do
         @eval using AbbreviatedStackTraces
