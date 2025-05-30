0f43987e19bb1290b6718a662da0d56c544a6a44
abhro <5664668+abhro@users.noreply.github.com>
2025-05-30 15:24:50 -0400

    Create jupyter_qtconsole_config.py

---
+++ jupyter_qtconsole_config.py
@@ -0,0 +1,10 @@
+# Configuration file for qtconsole.
+
+c = get_config()  #noqa
+
+c.ConsoleWidget.font_family = 'Cascadia Mono'
+c.ConsoleWidget.font_size = 14
+
+#c.ConsoleWidget.kind = 'rich'
+
+c.JupyterQtConsoleApp.confirm_exit = False
