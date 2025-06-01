7a62918263ff6b0fa005c4b885df604c358b09e6
abhro <5664668+abhro@users.noreply.github.com>
2025-06-01 12:35:59 -0400

    Update jupyter_qtconsole_config.py

--- jupyter_qtconsole_config.py
+++ jupyter_qtconsole_config.py
@@ -1,8 +1,10 @@
 # Configuration file for qtconsole.
 
+from platform import system
+
 c = get_config()  #noqa
 
-c.ConsoleWidget.font_family = 'Cascadia Mono'
+c.ConsoleWidget.font_family = 'Cascadia Mono'  if system() == 'Windows' else 'Hack'
 c.ConsoleWidget.font_size = 14
 
 #c.ConsoleWidget.kind = 'rich'
